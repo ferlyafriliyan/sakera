cedf718c2622e6d2b679fbd3a831c7a8f1fb543b
Denventa - AFR <122451961+Denventa@users.noreply.github.com>
2023-03-13 19:41:45 +0700

    Update sakera.py

--- sakera.py
+++ sakera.py
@@ -409,7 +409,7 @@ def scrap_useragent():
     data_ua = {}
     pt = 0
     pilih_device()
-    dc = input('   %s└──> %s'%(A,J))
+    dc = input('   %s└──> %s'%(A,H))
     if dc in ['1','01','a']:     type = 'software_name/samsung-browser'
     elif dc in ['2','02','b']:   type = 'software_name/nokia-browser'
     elif dc in ['3','03','c']:   type = 'operating_platform_string/xiaomi-mi-a1'
