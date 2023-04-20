0b5603fd625ddaa4f5e538b07fc7a166a2431297
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-04-20 09:11:23 +0700

    Update sakera.py

--- sakera.py
+++ sakera.py
@@ -397,7 +397,7 @@ def tampilan_menu():
 
   {J2}[{A2}16{J2}] {A2}Cek Hasil  {J2}[{A2}19{J2}] {P2}User Agent   {J2}[{A2}22{J2}] {A2}Akun Page
   {J2}[{A2}17{J2}] {A2}Cek Opsi   {J2}[{A2}20{J2}] {A2}Upgrade Pro  {J2}[{A2}23{J2}] {A2}Share Post
-  {J2}[{A2}18{J2}] {A2}Cek Teman  {J2}[{A2}21{J2}] {A2}Crack Nomor  {J2}[{A2}00{J2}] {P2}Log Out"""
+  {J2}[{A2}18{J2}] {A2}Cek Teman  {J2}[{A2}21{J2}] {A2}Crack Nomor  {J2}[{A2}00{J2}] {P2}Log Out {U2}[Cookie{U2}]"""
         printer(Panel(tampilan_menu,title=f'{J2}[ {P2}Menu {J2}]',subtitle=f'{A2}┌─ {J2}[ {P2}Pilih {J2}]',subtitle_align='left',width=54,padding=1,style='#FF8F00'))
         pilih_menu()
     except requests.exceptions.ConnectionError:print('\n   %s[%s•%s] %sTidak Ada Koneksi Internet %s!%s\n'%(M,P,M,P,M,P));exit()
