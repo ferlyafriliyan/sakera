bdd6311c1c8e53c6f22cad2506402928f88e7a51
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-04-26 21:00:44 +0700

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ Facebook = 'Facebook.com/Denventa.Xayonara.Team.UnlimitedARMY'
 Develpr = 'Denventa Ferly Afriliyan' 
 Version = '0.7' 
 Dnventa = 100013275378835 
-Post_Dev = 1643042226148274 
+Post_Dev = 1655893724863124 
 ###----------[ AUTHOR & CREATOR ]---------- ###
 # ------ [ Gausah Dioprek Ntar Error ] ------ #
 Author    = 'Dapunta Khurayra X'
