20ddbe1a99eeae554359e2fb2467898c332b7ba9
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-05-02 21:04:59 +0700

    Update run.py

--- run.py
+++ run.py
@@ -101,7 +101,7 @@ ugen = []
 uakuh = []
 usragent = []
 
-	try:
+try:
 		
 		prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=80000&country=all&ssl=all&anonymity=all').text	
 		open('.prox.txt','w').write(prox)
